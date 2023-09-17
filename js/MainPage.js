b27906b615c2cda3cf172faf2e30d57a20c30ea2
Ernes Raikulov <eraykulov@gmail.com>
2023-09-17 19:38:22 +0600

    update css and add mainpage

---
+++ js/MainPage.js
@@ -0,0 +1,12 @@
+
+
+const uploadExcelFile= () => {
+    const fileInput = document.getElementById('excelFile');
+    const file = fileInput.files[0];
+
+    if (file) {
+        console.log('Selected file:', file.name);
+    } else {
+        alert('Please select an Excel file.');
+    }
+}
