2c923671fb042426389c66fab0c95ebccf9a84de
Ernes Raikulov <eraykulov@gmail.com>
2023-09-17 19:14:42 +0600

    add accordion

--- js/Accordion.js
+++ js/Accordion.js
@@ -1,11 +1,14 @@
 const acc = document.getElementsByClassName("accordion");
-const i;
+let i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
     this.classList.toggle("active");
 
-    const panel = this.nextElementSibling;
+    /* Toggle between hiding and showing the active panel */
+    let panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
     } else {
