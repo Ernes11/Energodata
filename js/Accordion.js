4e3f06bce552ada6b6eed6c2debc1226cb156a32
Ernes Raikulov <eraykulov@gmail.com>
2023-09-17 18:27:32 +0600

    modify js

--- js/Accordion.js
+++ js/Accordion.js
@@ -4,8 +4,8 @@ const i;
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    
-    var panel = this.nextElementSibling;
+
+    const panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
     } else {
